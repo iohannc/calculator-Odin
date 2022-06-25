4eedb689851b242ce1c3025b96a7b7e2617f89b3
Juan Negrete <juan.negrete.dev@gmail.com>
2022-06-25 14:59:04 -0500

    Add very basic initial js functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function addition(n1, n2) {
+  return n1 + n2;
+}
+
+function substraction(n1, n2) {
+  return n1 - n2;
+}
+
+function multiplication(n1, n2) {
+  return n1 * n2;
+}
+
+function division(n1, n2) {
+  return n1 / n2;
+}
+
+function operate(n1, n2, operation) {
+  return operation(n1, n2);      
+}
